ba23c85c38206de591001940470afcb620b45a77
geeta <raigeeta654@gmail.com>
2025-08-27 08:49:18 +0500

    Add Largest_of_two_numbers solution

---
+++ Largest_of_two_numbers.js
@@ -0,0 +1,13 @@
+// 1) storing two numbers in varibales 
+// 2) comparing the numbers with eachother 
+// 3) displaying the largest number 
+
+let firstNumber = 0;
+let secondNumber = 12;
+
+if(firstNumber > secondNumber){
+    console.log(`The first number ${firstNumber} is greter then the second number ${secondNumber}`);
+}
+else{
+    console.log(`The second number ${secondNumber} is greter then the first number ${firstNumber}`);
+}
